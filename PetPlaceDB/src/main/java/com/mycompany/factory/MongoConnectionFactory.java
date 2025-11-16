e8b43a06bf3cb194809583fe466c4792546e8cd3
Gustavo Ferreira <Gustavo.nferreira@a.ucb.br>
2025-11-15 22:29:27 -0300

    chore: Adicionando o pacotes MVC;

---
+++ PetPlaceDB/src/main/java/com/mycompany/factory/MongoConnectionFactory.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.factory;
+
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class MongoConnectionFactory {
+    
+    // SUA CONNECTION STRING DO ATLAS
+    private static final String URI = 
+        "mongodb+srv://Gnferreira:admin123@petplacebd.82fyloi.mongodb.net/?appName=PetPlaceBD";
+
+    private static MongoClient client;
+
+    public static MongoDatabase getDatabase() {
+        if (client == null) {
+            client = MongoClients.create(URI);
+        }
+        return client.getDatabase("PetPlaceBD");  // nome do seu banco
+    }
+}
+
+    
+
